fe3aac1ad7cae13a8572f3d97d8f1d5ea6dc66dd
Angel Concepcion <angelconcepcion50@gmail.com>
2022-07-13 12:18:50 -0400

    solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         // your code here
+        Logger logger = Logger.getInstance();
+
+        logger.log("Sample output line " + 1);
+        logger.log("Second line of sample output.");
     }
 }
